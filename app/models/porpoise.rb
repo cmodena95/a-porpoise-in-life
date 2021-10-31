2f17bc1b293d4eacba2ea079eeb87f86cb1d74d9
Camila Modena <itscamilla@hotmail.com>
2021-10-31 18:50:52 +0100

    Added validations

--- app/models/porpoise.rb
+++ app/models/porpoise.rb
@@ -1,3 +1,10 @@
 class Porpoise < ApplicationRecord
   belongs_to :user
+  has_many :bookings, dependent: :destroy
+  has_many :reviews, through: :bookings
+
+  valdiates :name, :location, :species, presence: true
+  validates :price, numericality: { greater_than: 0 }
+
+  # validates :description, length: { minimum: 25 }
 end
