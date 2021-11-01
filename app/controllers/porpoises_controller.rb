d129da35990a97f137ebc8726bd250659406dd21
Camila Modena <itscamilla@hotmail.com>
2021-11-01 14:29:22 +0100

    Pundit working for index

--- app/controllers/porpoises_controller.rb
+++ app/controllers/porpoises_controller.rb
@@ -1,7 +1,5 @@
 class PorpoisesController < ApplicationController
-
     def index
-        @porpoises = Porpoise.all
+        @porpoises = policy_scope(Porpoise).order(created_at: :desc)
     end
-
 end
